0603e0799bf94d94e99a2b137dd920c6994a6447
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-07-12 14:42:47 -0300

    test(ds/header): adding test for logo rendering

--- src/components/ds/header/header.spec.tsx
+++ src/components/ds/header/header.spec.tsx
@@ -2,9 +2,14 @@ import { render, screen } from '@testing-library/react'
 import { Header } from './'
 
 describe('Header', () => {
-  it('renderiza header', () => {
+  it('renderiza o header', () => {
     render(<Header />)
     const element = screen.getByRole('banner')
     expect(element).toBeInTheDocument()
+  })
+
+  it('renderiza o logo', () => {
+    render(<Header />)
+    expect(screen.getByTestId('logo')).toBeInTheDocument()
   })
 })
